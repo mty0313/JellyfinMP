626b98f436b38ad2f79cbe03d64b9627724d872d
mty0313 <mty0313@126.com>
2024-06-01 12:14:12 +0800

    @DependsOn("dataSourceInitializer")

--- src/main/java/top/mty/service/RemoteServerInfoService.java
+++ src/main/java/top/mty/service/RemoteServerInfoService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@DependsOn("dataSourceInitializer")
 public class RemoteServerInfoService extends ServiceImpl<RemoteServerInfoMapper, RemoteServerInfo> {
   @Autowired
   private RemoteServerInfoMapper remoteServerInfoMapper;
